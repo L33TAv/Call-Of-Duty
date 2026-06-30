147277bf1f7229f8052239091cf8c1aace2d555e
L33TAv <leostam100@gmail.com>
2026-06-30 12:20:38 +0300

    Intial commit

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const {MongoClient} = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+const app = express();
+
+const pino = require('pino');
+const logger = pino();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+async function start(){
+
+  await client.connect();
+
+  const server = app.listen(PORT, () => {
+    logger.info('Server is running on port ${PORT}');
+  });
+
+  server.on('close', async () => {
+    await client.close();
+    logger.info('server was closed');
+  });
+
+}
+
+start();
