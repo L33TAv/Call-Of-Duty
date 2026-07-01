d5e5c46476ef9aebacc1b677c16ebec0c608613b
L33TAv <leostam100@gmail.com>
2026-07-01 16:30:20 +0300

    refactor+feat(health endpoints): add mockClient and mockBrockenClient, to be accustomed to change of createApp function export + add health/db endpoint test

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,12 +2,40 @@ import {expect,describe, it} from 'vitest';
 
 import request from 'supertest';
 
-import app from '../src/app.js'
+import createApp from '../src/app.js'
+
+const mockClient = {
+    db: ()=>
+        ({
+            command:async () => ({ok:1})
+        })
+};
+
+const mockBrokenClient = 
+{
+  db: () => 
+    ({
+    command: async () => {throw new Error('connection lost')}
+    })
+};
+
+const app = createApp(mockClient); 
+const badApp = createApp(mockBrokenClient);
 
 describe('checks that health endpoints works correctly',() => {
     it("checks if /health returns status code 200", async () => {
         const response = await request(app).get('/health');
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({status:'ok'});
-    })
-})
+    });
+
+    it("checks if /health/db returns status code 200 if the server is connected to the DB client properly", async () => {
+        const response = await request(app).get('/health/db');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({status:'ok'});
+
+        const badResponse = await request(badApp).get('/health/db');
+        expect(badResponse.statusCode).toBe(500);
+        expect(badResponse.body.status).toBe('error');
+    });
+});
