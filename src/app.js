7373cf4fecf73d0f038eb5c562d753798f163a44
L33TAv <leostam100@gmail.com>
2026-07-07 14:14:45 +0300

    chore(solider schema): add solider schema using zod

--- src/app.js
+++ src/app.js
@@ -4,6 +4,37 @@ import config from "./config.js";
 
 const logger = pino({level:config.logLevel});
 
+import * as z from "zod";
+
+const RANK_NAMES = {
+	0 : "private",
+	1 : "corporal",
+	2 : "sergeant",
+	3 : "lieutenant",
+	4 : "captain",
+	5 : "major",
+	6 : "colonel",
+}
+
+const soliderSchema = z.object({
+	_id : z.string().regex(/^\d+$/, {message: "the id must contain only numbers."}).length(7),
+	name :  z.string().min(3).max(50),
+	rankValue : z.number().gte(0).lte(6).optional(),
+	rankName : z.string().optional(),
+	limitations: z.array(z.string()).optional(),
+}).strict().refine((data) =>{
+	const rankValue = data["rankValue"];
+	const rankName = data["rankName"];
+	
+	if ((rankValue !== null && rankValue !== undefined) && (rankName !== null && rankName !== undefined))
+		return RANK_NAMES[rankValue] === rankName;
+	else if (rankName !== null && rankName !== undefined)
+		return Object.values(RANK_NAMES).includes(rankName);
+	return true
+},{
+	error:"rankName or rankValue doesn't match the requirements."
+});
+
 function createApp(client) {
 	const app = express();
 
