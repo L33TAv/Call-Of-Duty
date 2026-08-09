e5eb1e12a5b34cd7d47386bb872bbb37fc00817f
Did you not change your name? <this_is_not_an_email.com>
2026-08-09 14:19:36 +0300

    test(app): change testing using vi.spyOn, change description comments.

--- test/app.test.js
+++ test/app.test.js
@@ -1,40 +1,59 @@
+import { MongoNetworkError } from "mongodb";
 import request from "supertest";
-import { describe, expect, it } from "vitest";
-
+import {
+	afterAll,
+	afterEach,
+	beforeAll,
+	describe,
+	expect,
+	it,
+	vi,
+} from "vitest";
 import createApp from "../src/app.js";
+import * as client from "../src/db/client.js";
 
-const mockClient = {
-	db: () => ({
-		command: async () => ({ ok: 1 }),
-	}),
-};
+const app = createApp();
 
-const mockBrokenClient = {
-	db: () => ({
-		command: async () => {
-			throw new Error("connection lost");
-		},
-	}),
-};
+beforeAll(async () => {
+	await client.connectClient();
+});
 
-const app = createApp(mockClient);
-const badApp = createApp(mockBrokenClient);
+afterEach(() => {
+	vi.restoreAllMocks();
+});
 
-describe("Test /health endpoints", () => {
-	it("should return code 200 for /health endpoint", async () => {
+afterAll(async () => {
+	await client.getDb().dropDatabase();
+
+	const mongoClient = await client.connectClient();
+	await mongoClient.close();
+});
+
+describe("Test health endpoints", () => {
+	it("should return code 200 for /health", async () => {
 		const response = await request(app).get("/health");
 		expect(response.statusCode).toBe(200);
 		expect(response.body).toEqual({ status: "ok" });
 	});
 
-	it("should return status code 200 for /health/db get route", async () => {
+	it("should return status code 200 for GET /health/db", async () => {
 		const response = await request(app).get("/health/db");
 		expect(response.statusCode).toBe(200);
 		expect(response.body).toEqual({ status: "ok" });
 	});
 
-	it("should return status code 500 when fails to ping /health/db get route", async () => {
-		const badResponse = await request(badApp).get("/health/db");
+	it("should return status code 500 when fails to ping GET /health/db", async () => {
+		vi.spyOn(client, "connectClient").mockRejectedValue({
+			db: () => ({
+				command: async () => {
+					throw new MongoNetworkError(
+						"failed to connect to server on first connect",
+					);
+				},
+			}),
+		});
+
+		const badResponse = await request(app).get("/health/db");
 		expect(badResponse.statusCode).toBe(500);
 		expect(badResponse.body.status).toBe("error");
 	});
